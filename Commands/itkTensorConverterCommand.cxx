1883213df81991d82f820f003a96f2ffb1325942
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-07-02 15:57:17 +0100

    Adding an option to the converter to read and write  tensors as 4D files as it is the format used in DTIStudio and other tensor processing software

--- Commands/itkTensorConverterCommand.cxx
+++ Commands/itkTensorConverterCommand.cxx
@@ -22,16 +22,14 @@
 
 #include <iostream>
 
-
 namespace itk
 {
-
 
   TensorConverterCommand::TensorConverterCommand()
   {
     m_ShortDescription = "Convert a tensor image into another format";
     m_LongDescription = "Usage:\n";
-    m_LongDescription += "<file in> <file out>\n\n";
+    m_LongDescription += "<file in> <file out> [UseFSLStyle (boolean, default = 0)]\n\n";
     m_LongDescription += m_ShortDescription;
   }
 
@@ -41,19 +39,23 @@ namespace itk
   int TensorConverterCommand::Execute (int narg, const char* arg[])
   {
     
-    if( narg != 3 )
+    if( narg < 3 )
     {
       std::cout << this->GetLongDescription() << std::endl;
       return -1;
     }
 
-  
+    const char* input = arg[1];
+    const char* output = arg[2];
+
+    unsigned int usefsl = narg > 3 ? std::atoi (arg[3]) : 0;
+    
     // write the output
     typedef itk::TensorImageIO<double, 3, 3> IOType;
     
-    
     IOType::Pointer myReader = IOType::New();
-    myReader->SetFileName(arg[1]);
+    myReader->SetFileName(input);
+    
     try
     {
       myReader->Read();
@@ -63,13 +65,13 @@ namespace itk
       std::cerr << e;
       return -1;
     }
-
     
     IOType::Pointer myWriter = IOType::New();
-    myWriter->SetFileName(arg[2]);
+    myWriter->SetFileName(output);
+    myWriter->SetUseFSLStyle (usefsl);
     myWriter->SetInput( myReader->GetOutput() );
     
-    std::cout << "Writing: " << arg[2] << std::flush;
+    std::cout << "Writing: " << output << std::flush;
     try
     {
       myWriter->Write();
